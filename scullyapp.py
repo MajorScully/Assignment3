a6a47996078fafb590500ff3a206c1ea53d598a0
MajorScully <83037819+MajorScully@users.noreply.github.com>
2024-04-11 13:25:48 -0400

    Update scullyapp.py

--- scullyapp.py
+++ scullyapp.py
@@ -1,31 +1,9 @@
-#from flask import Flask
-#app = Flask(__name__)
-
-#@app.route('/')
-#def hello_cloud():
-#  return 'Hello from Scullys ECS container!'
-
-#if __name__ == "__main__":
-#    app.run(host='0.0.0.0', port=5000)
-
-
-from flask import Flask, request
- 
+from flask import Flask
 app = Flask(__name__)
- 
+
 @app.route('/')
-def main_route():
-    return 'Main route data display'
- 
-@app.route('/host')
-def host_route():
-    return 'Host route data display'
- 
-@app.route('/ip')
-def ip_route():
-    # Get client's IP address from the request object
-    client_ip = request.remote_addr
-    return f'IP route data display. Client IP: {client_ip}'
- 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+def hello_cloud():
+  return 'Hello from Scullys ECS container!'
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
